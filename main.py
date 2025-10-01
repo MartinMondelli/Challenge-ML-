b64b3dbcd5a693c725eab1e5defbbe919d3b47cd
MartinMondelli <mondelli.e.martin@gmail.com>
2025-10-01 09:54:48 +0200

    Upadate 1

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 import pandas as pd
+from sklearn.ensemble import RandomForestClassifier, RandomForestRegressor
+import sklearn as sk
 
 ## Import data as a panda dataframe
 df_train_in = pd.read_csv("https://edouardpauwels.fr/MLM2DSSS/challenge_train_features.csv",index_col=0)
@@ -69,3 +71,23 @@ def clean_dummies (df):
   #Director lo podemos hacer con crew, util?? =======================================
   return x_dum
 df_train_in_2 = clean_dummies(df_train_in)
+df_test_processed = clean_dummies(df_test)
+
+df_train_run = df_train_in_2[["sequels", "star", "season_horror", "season_romance", "season_family",
+                            "star", "big_comp"]]
+df_test_run = df_test_processed[["sequels", "star", "season_horror", "season_romance", "season_family",
+                            "star", "big_comp"]]
+
+param_clfrf = {
+    'max_depth':[8,10,20,25,34,40,45]     # Max deep of each tree
+}
+
+clfrf = RandomForestRegressor()
+clfrf_cv = GridSearchCV(
+    estimator=clfrf,
+    param_grid=param_clfrf,
+    cv=5,  # 5-fold cross-validation
+    scoring='accuracy',  # Optimizar por precisión
+    verbose=1  # Mostrar progreso
+)
+y_output = clfrf_cv.fit(df_train_run, y_train).predict(df_test_run)
